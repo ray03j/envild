486bfd90328be9898549d26d158c988f8cd02b1c
ray03j <eihouse.potato@outlook.jp>
2024-01-26 17:10:50 +0900

    [fix] design improved

--- app/ui/PostCard.tsx
+++ app/ui/PostCard.tsx
@@ -2,18 +2,20 @@ import { PostInfo } from "@/app/lib/definitions";
 
 const PostCard = (postInfo: PostInfo) => {
   return (
-    <div className="m-2 w-64 h-32 rounded bg-gray-300 text-black">
+    <div className="m-2 w-64 h-42 rounded bg-gray-300 text-black p-4">
       <div>
-        <div className="text-3xl">{postInfo.title}</div>
-        <div className="text-2xl">{postInfo.envTag}</div>
+        <div className="text-2xl font-bold mb-2">{postInfo.title}</div>
+        <div className="text-xl">{postInfo.envTag}</div>
       </div>
-        <div className="flex">
-          {postInfo.tagsComponent && 
+      <div className="flex mt-2">
+        {postInfo.tagsComponent && 
           postInfo.tagsComponent.map((tag, i) => (
-            <div key={i} className="m-1">{tag}</div>
+            <div key={i} className="mr-2 bg-gray-200 p-1 rounded">{tag}</div>
           ))}
-        </div>
-        <div>{postInfo.dateTime ? postInfo.dateTime.toLocaleString(): "" }</div>
+      </div>
+      <div className="mt-2 text-gray-600">
+        {postInfo.dateTime ? postInfo.dateTime.toLocaleString() : ""}
+      </div>
     </div>
   );
 };
