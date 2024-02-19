575e69b931a220d6091588e4fbc204c1620e4ecc
ray03j <eihouse.potato@outlook.jp>
2024-02-19 13:01:21 +0900

    [add] markdown ui

--- app/posts/[id]/edit/page.tsx
+++ app/posts/[id]/edit/page.tsx
@@ -1,51 +1,46 @@
 'use client'
 
-import { useFormState } from "react-dom";
-import { State, createPost } from "@/app/lib/actions";
-import Link from "next/link";
-import { PostPreview } from "@/app/lib/definitions";
-import { CreatePost } from "@/app/ui/posts/buttons";
+import { useState } from 'react';
+import SimpleMde from 'react-simplemde-editor';
+import "easymde/dist/easymde.min.css";
+
 
 export default function Form() {
-  const initialState = { message: null, errors: {} };
-  const [state, dispatch] = useFormState<State, FormData>(createPost, initialState);
-
+  const [content, setContent] = useState('');
+  
+  const [markdown, setMarkdown] = useState('');
+  
+  const handleChange = (v) => {
+    setContent(v);
+  };
+  
+  // const [previewMode, setPreviewMode] = useState(false);
+  // const togglePreview = () => {
+  //   setPreviewMode(!previewMode);
+  // };
+  
   return (
-    <form className="max-w-md mx-auto p-6 bg-white shadow-md rounded-lg" action={dispatch}>
-      <h1 className="text-xl font-bold mb-4 text-black">Create Post</h1>
+    <div className="max-w-screen-md mx-auto px-4 py-8">
+      <div className="bg-white rounded-lg shadow-md p-6">
+        <h1 className="text-3xl font-bold mb-4 text-gray-800">Create Post</h1>
 
-      <div className="mb-4">
-        <label htmlFor="content" className="block text-sm font-semibold mb-1 text-black">Content</label>
-        <textarea
-          id="content"
-          name="content"
-          rows={6} // テキストエリアの高さを設定
-          className="w-full border border-gray-300 rounded px-3 py-2 focus:outline-none focus:border-blue-500 text-black"
-        />
-        <div aria-live="polite" aria-atomic="true">
-          {state.errors?.content &&
-            state.errors.content.map((error: string) => (
-              <p className="mt-2 text-sm text-red-500" key={error}>
-                {error}
-              </p>
-            ))}
+        
+        <div className="mb-8">
+          <label htmlFor="content" className="block text-lg font-semibold mb-2 text-gray-800">Content</label>
+          <SimpleMde
+            id="content"
+            value={content}
+            onChange={handleChange}
+            className="text-gray-800"
+            placeholder="Write your article here..."
+          />
         </div>
-      </div>
 
-      <div className="mb-4">
-        <button type="submit" className="bg-blue-500 text-white px-4 py-2 rounded hover:bg-blue-600">Create Post</button>
-        {state.message && (
-          <div className="text-red-600 text-sm" aria-live="polite">
-            {state.message}
-          </div>
-        )}
-      </div>
+        <div className="mb-8 flex justify-between">
+          <button type="submit" className="bg-blue-500 text-white px-6 py-2 rounded hover:bg-blue-600">Create Post</button>
+        </div>
+    </div>
 
-      <div>
-        <Link href="/" className="flex h-10 items-center rounded-lg bg-gray-100 px-4 text-sm font-medium text-gray-600 transition-colors hover:bg-gray-200">
-          Cancel
-        </Link>
       </div>
-    </form>
   );
 }
